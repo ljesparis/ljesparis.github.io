3388d430d45fe80337c50397e75a236f3f70c612
Leonardo Esparis <ljesparis@gmail.com>
2022-08-06 14:32:53 +0200

    refactor: mobile first added
    Right now the app has a basic mobile first design.
    
    Resolve #1

--- src/App.vue
+++ src/App.vue
@@ -26,58 +26,49 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="header">
-    <n-grid cols="1 m:2" responsive="screen">
-      <n-gi>
-        <div class="header__title">
-          <p>ljesparis</p>
-        </div>
-      </n-gi>
-      <n-gi>
-        <div class="header__extra">
-          <n-button tertiary circle style="color:white" @click="onGithubClick()">
-            <template #icon>
-              <n-icon size="32">
-                <logo-github />
-              </n-icon>
-            </template>
-          </n-button>
-          <n-button tertiary circle style="color:white" @click="onLinkedinClick()">
-            <template #icon>
-              <n-icon size="32">
-                <logo-linkedin />
-              </n-icon>
-            </template>
-          </n-button>
-        </div>
-      </n-gi>
-    </n-grid>
-  </div>
-  <div class="body">
-    <p>I'm <u>Leonardo</u>, a python backend developer. at the beginning of my career I started developing applications
-      on the frontend (only 1 year and 9 months) and ended up moving to the backend. In the last
-      almost three years, I have focused on working with companies that care about clean code, applying the SOLID
-      principles, TDD,
-      CLEAN
-      ARCHITECTURE and good practices.</p>
-  </div>
+  <n-grid cols="1 m:2" responsive="screen">
+    <n-gi class="header__title">
+      <p>ljesparis</p>
+    </n-gi>
+    <n-gi class="header__extra">
+      <n-button tertiary circle style="color:white" @click="onGithubClick()">
+        <template #icon>
+          <n-icon size="32">
+            <logo-github />
+          </n-icon>
+        </template>
+      </n-button>
+      <n-button tertiary circle style="color:white" @click="onLinkedinClick()">
+        <template #icon>
+          <n-icon size="32">
+            <logo-linkedin />
+          </n-icon>
+        </template>
+      </n-button>
+    </n-gi>
+    <n-grid-item class="body__content">
+      <p>I'm <u>Leonardo</u>, a python backend developer. at the beginning of my career I started developing
+        applications
+        on the frontend (only 1 year and 9 months) and ended up moving to the backend. In the last
+        almost three years, I have focused on working with companies that care about clean code, applying the SOLID
+        principles, TDD,
+        CLEAN
+        ARCHITECTURE and good practices.</p>
+    </n-grid-item>
+  </n-grid>
 </template>
 
 <style scoped>
-.header {
-  padding-top: 1.6em;
+.n-grid {
+  margin-top: 2.4em;
+}
+
+.header__title,
+.header__extra {
   display: flex;
-  justify-content: center;
   align-items: center;
-}
-
-.header .n-grid {
-  max-width: 58%;
-}
-
-.header__title {
-  display: flex;
-  justify-content: start;
+  justify-content: center;
+  justify-content: center;
 }
 
 .header__title p {
@@ -87,43 +78,32 @@ export default defineComponent({
   margin: 0;
 }
 
-.header__extra {
-  display: flex;
-  justify-content: end;
-  align-items: center;
-  height: 100%;
-}
-
 .header__extra button:last-child {
   margin-left: 1em;
 }
 
-.body {
-  position: absolute;
-  top: 39%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+.body__content {
+  grid-column: span 1 / span 1;
+  margin-left: 0;
 }
 
-.body p {
-  font-size: 2em;
+.body__content p {
+  padding: 0 2em 0 2em;
+  font-size: 24px;
+  margin-top: 3em;
+  text-align: justify;
 }
 
-@media (max-width: 1023px) {
 
-  .header .n-grid {
-    max-width: 100%;
+@media (min-width: 1023px) {
+
+  .body__content {
+    width: 100%;
+    grid-column: span 1 / span 1;
+    margin-left: calc(48.4% + 0px);
   }
 
-  .header__title {
-    justify-content: center;
-    align-items: center;
-    padding: 0;
-  }
-
-  .header__extra {
-    justify-content: center;
-    align-items: center;
+  .body__content p {
     padding: 0;
   }
 }
