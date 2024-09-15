82d961e88ca4f2c4039cb49b9d25ef4a5a042b50
Leonardo Esparis <ljesparis@gmail.com>
2024-09-15 18:06:34 +0200

    rename headerlink component to link (#11)

--- src/consts.ts
+++ src/consts.ts
@@ -2,15 +2,20 @@ export const SITE_TITLE = 'ljesparis (Leonardo Esparis)';
 export const SITE_HTML_TITLE = 'Welcome to my site, btw, i\'m Leonardo';
 export const SITE_DESCRIPTION = 'Welcome to my site, i\'m Leonardo Esparis a passionate Software Developer =). I\'ll explain here about me self and i\'ll provide you with my github and linkedin urls';
 
-
-export const SITE_EXTERNAL_LINKS = [
+export const ROUTES = [
     {
-        link: "https://www.linkedin.com/in/leoesparis/",
-        name: "LinkedIn"
+        pathOrUrl: '/',
+        title: 'Home',
+        isLocal: true
     },
     {
-        link: "https://github.com/ljesparis/",
-        name: "Github"
+        pathOrUrl: "https://www.linkedin.com/in/leoesparis/",
+        title: "LinkedIn",
+        isLocal: false
+    },
+    {
+        pathOrUrl: "https://github.com/ljesparis/",
+        title: "Github",
+        isLocal: false
     }
-];
-
+]
