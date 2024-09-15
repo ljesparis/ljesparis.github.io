5c83d66ae3d74cc09bae13a054b8d896cf66be5a
Esparis <ljesparis@gmail.com>
2024-09-15 18:12:44 +0200

    change subtitle, description and also remove .vscode directory

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 export const SITE_TITLE = 'ljesparis (Leonardo Esparis)';
-export const SITE_HTML_TITLE = 'Welcome to my site, btw, i\'m Leonardo';
+export const SITE_HTML_TITLE = 'Hey there! I\'m Leonardo—welcome to my site!';
 export const SITE_DESCRIPTION = 'Welcome to my site, i\'m Leonardo Esparis a passionate Software Developer =). I\'ll explain here about me self and i\'ll provide you with my github and linkedin urls';
 
 export const ROUTES = [
