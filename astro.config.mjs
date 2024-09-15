8bad53706ec6750bfad9844a9f295412500dc39b
Esparis <ljesparis@gmail.com>
2024-09-15 20:40:50 +0200

    configure astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
     site: 'https://ljesparis.com/',
+    compressHTML: true,
     integrations: [mdx(), sitemap()],
+    prefetch: {
+        defaultStrategy: "load"
+    },
 });
